e1cc08c187d5bf33198d4c6310e3f7505f5375e2
Oleksandr Volos <volosbest10@gmail.com>
2026-01-13 21:02:02 +0200

    added responsive style

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelectorAll('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpen.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
+modalBtnClose.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
